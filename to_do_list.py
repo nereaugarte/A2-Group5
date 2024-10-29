6bed60341d4d0bef98fa445f25ba0201f7016582
kimlaureen <kimlaureen.schafer@alumni.esade.edu>
2024-10-29 18:01:15 +0100

    Implementing Feature 1 to add task and delete

---
+++ to_do_list.py
@@ -0,0 +1,13 @@
+
+tasks = []
+
+
+def add_task(task):
+
+    tasks = tasks + [task] 
+    print(f"Added task: {task}")
+
+
+def delete_task(task_number):
+    task = tasks.pop(task_number)  
+    print(f"Deleted task: {task}"
